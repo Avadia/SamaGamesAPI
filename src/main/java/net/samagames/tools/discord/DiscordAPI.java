2a22dd71c909fa78ae507f7b7a617bea4f141b34
FarmVivi <farmvividev@gmail.com>
2020-09-06 11:16:31 +0200

    DiscordAPI: UUID_LIST revert ; add mute and unmute feature

--- src/main/java/net/samagames/tools/discord/DiscordAPI.java
+++ src/main/java/net/samagames/tools/discord/DiscordAPI.java
@@ -26,7 +26,7 @@ import java.util.logging.Level;
  * along with SamaGamesAPI.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class DiscordAPI {
-    private static final int TIMEOUT = 20000;
+    private static final int TIMEOUT = 15000;
     private static int generator = 0;
     private static final Map<Integer, MutablePair<ResultType, Object>> results = new HashMap<>();
 
@@ -93,6 +93,38 @@ public class DiscordAPI {
         return (List<UUID>) pair.getRight();
     }
 
+    public static List<UUID> mutePlayers(@Nonnull List<UUID> uuids) {
+        MutablePair<ResultType, Object> pair = MutablePair.of(ResultType.UUID_LIST, new ArrayList<>());
+        int id = generator++;
+        DiscordAPI.results.put(id, pair);
+        final String[] msg = {"mute"};
+        uuids.forEach(uuid -> msg[0] += ":" + uuid.toString());
+        DiscordAPI.publish(id, msg[0]);
+        try {
+            synchronized (pair) {
+                pair.wait(DiscordAPI.TIMEOUT);
+            }
+        } catch (Exception ignored) {
+        }
+        return (List<UUID>) pair.getRight();
+    }
+
+    public static List<UUID> unmutePlayers(@Nonnull List<UUID> uuids) {
+        MutablePair<ResultType, Object> pair = MutablePair.of(ResultType.UUID_LIST, new ArrayList<>());
+        int id = generator++;
+        DiscordAPI.results.put(id, pair);
+        final String[] msg = {"unmute"};
+        uuids.forEach(uuid -> msg[0] += ":" + uuid.toString());
+        DiscordAPI.publish(id, msg[0]);
+        try {
+            synchronized (pair) {
+                pair.wait(DiscordAPI.TIMEOUT);
+            }
+        } catch (Exception ignored) {
+        }
+        return (List<UUID>) pair.getRight();
+    }
+
     public static boolean isConnected(@Nonnull UUID player) {
         MutablePair<ResultType, Object> pair = MutablePair.of(ResultType.BOOLEAN, false);
         int id = generator++;
@@ -130,10 +162,14 @@ public class DiscordAPI {
                 String[] content = args[2].split(":");
                 switch (result.getLeft()) {
                     case UUID_LIST:
-                        List<UUID> uuid = new ArrayList<>();
-                        for (int i = 1; i < content.length; i++)
+                        List<UUID> uuid = (List<UUID>) result.getRight();
+                        for (int i = 1; i < content.length; i++) {
+                            if (content[i].equals("ERROR")) {
+                                SamaGamesAPI.get().getPlugin().getLogger().severe("[DiscordAPI] Error : " + args[2] + "(packet = " + packet + ")");
+                                continue;
+                            }
                             uuid.add(UUID.fromString(content[i]));
-                        result.setRight(uuid);
+                        }
                         break;
                     case LONG:
                         result.setRight(Long.parseLong(content[0]));
